aca893049718ac59fb4a1872aff6c5c74aa33980
guvvalaphani <guvvalaphani@gmail.com>
2024-03-21 22:53:47 +0000

    tiger

---
+++ sum.py
@@ -0,0 +1,9 @@
+# Input numbers
+num1 =int(input("Enter first number: "))
+num2 =int(input("Enter second number: "))
+
+# Add the numbers
+sum = num1 + num2
+
+# Print the result
+print("The sum of", num1, "and", num2, "is:", sum)
